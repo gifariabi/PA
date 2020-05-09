0dba3e5ead5244e9cd9aec10b820f50d8f749d97
faizazharr <faizazharr@gmail.com>
2020-05-10 01:12:26 +0700

    update js

--- application/views/laporan_kas.php
+++ application/views/laporan_kas.php
@@ -13,11 +13,11 @@
   <title></title>
 
   <!-- Custom fonts for this template-->
-  <link href="<?php echo base_url().'asset/vendor/fontawesome-free/css/all.min.css'?>" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url().'asset/masuk/vendor/fontawesome-free/css/all.min.css'?>" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="<?php echo base_url().'asset/css/sb-admin-2.min.css" rel="stylesheet'?>">
+  <link href="<?php echo base_url().'asset/masuk/css/sb-admin-2.min.css" rel="stylesheet'?>">
 
 </head>
 
@@ -533,20 +533,20 @@ function drawChart() {
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="<?php echo base_url().'asset/vendor/jquery/jquery.min.js'?>"></script>
-  <script src="<?php echo base_url().'asset/vendor/bootstrap/js/bootstrap.bundle.min.js'?>"></script>
+  <script src="<?php echo base_url().'asset/masuk/vendor/jquery/jquery.min.js'?>"></script>
+  <script src="<?php echo base_url().'asset/masuk/vendor/bootstrap/js/bootstrap.bundle.min.js'?>"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="<?php echo base_url().'asset/vendor/jquery-easing/jquery.easing.min.js'?>"></script>
+  <script src="<?php echo base_url().'asset/masuk/vendor/jquery-easing/jquery.easing.min.js'?>"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="<?php echo base_url().'asset/js/sb-admin-2.min.js'?>"></script>
+  <script src="<?php echo base_url().'asset/masuk/js/sb-admin-2.min.js'?>"></script>
 
   <!-- Page level plugins -->
-  <script src="<?php echo base_url().'asset/chartjs/Chart.min.js'?>"></script>
+  <script src="<?php echo base_url().'asset/masuk/chartjs/Chart.min.js'?>"></script>
 
   <!-- Page level custom scripts -->
-  <script src="<?php echo base_url().'asset/js/demo/chart-area-demo.js'?>"></script>
-  <script src="<?php echo base_url().'asset/js/demo/chart-pie-demo.js'?>"></script>
+  <script src="<?php echo base_url().'asset/masuk/js/demo/chart-area-demo.js'?>"></script>
+  <script src="<?php echo base_url().'asset/masuk/js/demo/chart-pie-demo.js'?>"></script>
 </body>
 </html>
